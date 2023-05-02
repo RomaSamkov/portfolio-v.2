526b78a7130d97e9147f2fdb6b18dd8e0e630db7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-02 21:28:20 +0300

    add some components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,18 @@
 import Navbar from "../Navbar/Navbar";
-import { Section } from "./Hero.styled";
+import { Container, LeftContent, RightContent, Section } from "./Hero.styled";
 
 function Hero() {
   return (
     <Section>
       <Navbar />
+      <Container>
+        <LeftContent>
+          <h1>React.TS.JS.Node.js</h1>
+          <img src="" alt="" />
+          <p>I am junior web developer</p>
+        </LeftContent>
+        <RightContent></RightContent>
+      </Container>
     </Section>
   );
 }
