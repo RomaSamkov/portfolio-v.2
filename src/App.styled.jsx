4dbd51efbfa2dba378441ca6d396d6b2f5bb39e5
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-02 21:27:40 +0300

    add change background color

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const Conatiner = styled.div`
   height: 100vh;
-  background-color: #41485c;
+  /* background-color: #41485c; */
+  background: rgb(60, 60, 18);
+  background: linear-gradient(
+    90deg,
+    rgba(60, 60, 18, 1) 0%,
+    rgba(21, 8, 46, 0.9023984593837535) 45%,
+    rgba(1, 2, 61, 1) 100%
+  );
   color: white;
   scroll-snap-type: y mandatory;
   scroll-behavior: smooth;
