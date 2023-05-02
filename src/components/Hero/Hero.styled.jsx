013eaf71bc8f912935ae828cbce2ba32007ffce6
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-02 21:29:36 +0300

    add Container, RightContent , LeftContent

--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -3,4 +3,20 @@ import styled from "styled-components";
 export const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `;
+
+export const Container = styled.div`
+  width: 1200px;
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const LeftContent = styled.div``;
+
+export const RightContent = styled.div``;
