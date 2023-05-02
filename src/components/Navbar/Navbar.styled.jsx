0df8c280f643ad3c493e5744a52394ae97150e06
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-02 21:29:55 +0300

    add styled to Button

--- src/components/Navbar/Navbar.styled.jsx
+++ src/components/Navbar/Navbar.styled.jsx
@@ -6,8 +6,8 @@ export const Section = styled.div`
 `;
 
 export const Conatiner = styled.div`
-  width: 1400px;
-  background-color: #46bd69;
+  width: 1200px;
+  /* background-color: #46bd69; */
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -19,23 +19,39 @@ export const Links = styled.div`
   align-items: center;
   gap: 80px;
 `;
+
 export const Logo = styled.div``;
+
 export const List = styled.ul`
   display: flex;
   gap: 20px;
 `;
-export const ListItem = styled.li``;
+
+export const ListItem = styled.li`
+  cursor: pointer;
+`;
+
 export const Icons = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   gap: 20px;
 `;
+
 export const Icon = styled.div`
   cursor: pointer;
 `;
+
 export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
   cursor: pointer;
   border: none;
   border-radius: 12px;
+  padding: 5px;
+  background-color: #e03ee6;
   &:hover {
     color: #fff;
     background-color: rebeccapurple;
