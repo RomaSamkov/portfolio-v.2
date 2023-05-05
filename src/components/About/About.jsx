4a51a7422cf525e6a77956fd17b7388996cd532e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-05 15:57:37 +0300

    add components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,28 @@
-import { Section } from "./About.styled";
+import {
+  Button,
+  Container,
+  LeftContent,
+  RightContent,
+  Section,
+  SubTitle,
+  Title,
+} from "./About.styled";
 
 function About() {
-  return <Section>About</Section>;
+  return (
+    <Section>
+      <Container>
+        <LeftContent>
+          {/* <Picture src="/img/laptop.png" alt="" /> */}
+        </LeftContent>
+        <RightContent>
+          <Title>Roma RS</Title>
+          <SubTitle>I am junior web developer</SubTitle>
+          <Button>See my Projects</Button>
+        </RightContent>
+      </Container>
+    </Section>
+  );
 }
 
 export default About;
