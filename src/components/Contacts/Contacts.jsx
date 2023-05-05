7d4e4ceec78e5afc727516ade7f19421b0ed1f87
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-05 21:22:06 +0300

    add handleSubmit

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,43 @@
-import { Section } from "./Contacts.styled";
+import {
+  ButtonForm,
+  Container,
+  Form,
+  FormTitle,
+  Input,
+  LeftContent,
+  RightContent,
+  Section,
+  TextArea,
+} from "./Contacts.styled";
 
 function Contacts() {
-  return <Section>Contacts</Section>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Section>
+      <Container>
+        <LeftContent>
+          <Form onSubmit={handleSubmit}>
+            <FormTitle>Contact Us</FormTitle>
+            <Input type="text" placeholder="Name" />
+            <Input type="email" name="" id="" placeholder="Email" />
+            <TextArea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Write your message"
+            ></TextArea>
+            <ButtonForm type="submit">Send</ButtonForm>
+          </Form>
+        </LeftContent>
+        <RightContent>
+          <img src="/img/1.png" alt="" />
+        </RightContent>
+      </Container>
+    </Section>
+  );
 }
 
 export default Contacts;
