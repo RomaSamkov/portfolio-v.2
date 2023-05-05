71a5e886a4a2c32f575d48b1d47f188ddb8856c7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-05 15:58:10 +0300

    add components

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,7 +1,30 @@
-import { Section } from "./Works.styled";
+import { data } from "../../data/data";
+import {
+  Container,
+  LeftContent,
+  RightContent,
+  Section,
+  WorkItem,
+  WorkList,
+} from "./Works.styled";
 
 function Works() {
-  return <Section>Works</Section>;
+  return (
+    <Section>
+      <Container>
+        <LeftContent>
+          <WorkList>
+            {data.map((item) => (
+              <WorkItem key={item} text={item}>
+                {item}
+              </WorkItem>
+            ))}
+          </WorkList>
+        </LeftContent>
+        <RightContent></RightContent>
+      </Container>
+    </Section>
+  );
 }
 
 export default Works;
