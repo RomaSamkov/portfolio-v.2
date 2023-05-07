eda45cd4473c47e2c06078b30b7e336022012fb1
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-07 19:32:10 +0300

    Create Cube.jsx

---
+++ src/components/Cube/Cube.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/no-unknown-property */
+import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+function Cube() {
+  const textRef = useRef();
+  useFrame(
+    (state) =>
+      (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2)
+  );
+  return (
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial>
+        <RenderTexture attach="map">
+          <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+          <color attach="background" args={["pink"]} />
+          <Text ref={textRef} fontSize={1} color={"#555"}>
+            Hello
+          </Text>
+        </RenderTexture>
+      </meshStandardMaterial>
+    </mesh>
+  );
+}
+
+export default Cube;
