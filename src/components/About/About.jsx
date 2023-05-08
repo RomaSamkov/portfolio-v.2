e91a3bdc09ebad2a7256966b3d77e100fc9efca1
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-08 13:21:40 +0300

    add Canvas

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import {
   Button,
   Container,
@@ -7,13 +8,21 @@ import {
   SubTitle,
   Title,
 } from "./About.styled";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Cube from "../Cube/Cube";
 
 function About() {
   return (
     <Section>
       <Container>
         <LeftContent>
-          {/* <Picture src="/img/laptop.png" alt="" /> */}
+          <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+            <OrbitControls enableZoom={false} autoRotate />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Cube />
+          </Canvas>
         </LeftContent>
         <RightContent>
           <Title>Roma RS</Title>
