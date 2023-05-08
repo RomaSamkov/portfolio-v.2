cd577d602114798206ae5d70c2c2b58026ffc61e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-08 13:21:50 +0300

    Update Cube.jsx

--- src/components/Cube/Cube.jsx
+++ src/components/Cube/Cube.jsx
@@ -11,13 +11,13 @@ function Cube() {
   );
   return (
     <mesh>
-      <boxGeometry args={[2, 2, 2]} />
+      <boxGeometry />
       <meshStandardMaterial>
         <RenderTexture attach="map">
-          <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
           <color attach="background" args={["pink"]} />
-          <Text ref={textRef} fontSize={1} color={"#555"}>
-            Hello
+          <Text ref={textRef} fontSize={4} color={"#555"}>
+            Hello:))!
           </Text>
         </RenderTexture>
       </meshStandardMaterial>
