8f5bbb7ba3188194e5feadae8fdaea451c56d0c1
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-08 16:58:32 +0300

    Create WebDesign.jsx

---
+++ src/components/WebDesign/WebDesign.jsx
@@ -0,0 +1,27 @@
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+import Mac from "../Mac/Mac";
+import { Desc } from "../Development/DEvelopment.styled";
+
+const WebDesign = () => {
+  return (
+    <>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Stage environment="city" intensity={0.6}>
+            <Mac />
+          </Stage>
+          <OrbitControls enableZoom={false} autoRotate />
+        </Suspense>
+      </Canvas>
+      <Desc>
+        We design products with a strong focus on both world class design and
+        ensuring your product is a market success.
+      </Desc>
+    </>
+  );
+};
+
+export default WebDesign;
