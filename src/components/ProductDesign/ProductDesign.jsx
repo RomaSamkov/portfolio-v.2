a73a38b08c185ab884f3feeb98439178e97e9571
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-08 16:58:22 +0300

    Create ProductDesign.jsx

---
+++ src/components/ProductDesign/ProductDesign.jsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react";
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+import Shoe from "../Shoe/Shoe";
+import { Desc } from "../Development/DEvelopment.styled";
+
+const ProductDesign = () => {
+  return (
+    <>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Stage environment="city" intensity={0.6}>
+            <Shoe />
+          </Stage>
+          <OrbitControls enableZoom={false} autoRotate />
+        </Suspense>
+      </Canvas>
+      <Desc>
+        We design products with a strong focus on both world class design and
+        ensuring your product is a market success.
+      </Desc>
+    </>
+  );
+};
+
+export default ProductDesign;
